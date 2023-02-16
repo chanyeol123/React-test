d95b54fb244a79b87d80ce2c9e03de5965544d9f
chanyeol123 <chanyeol123@gmail.com>
2023-02-16 11:41:02 +0900

    숙련주차 시험

---
+++ src/redux/modules/todos.js
@@ -0,0 +1,102 @@
+// Action value
+const ADD_TODO = "ADD_TODO";
+const GET_TODO_BY_ID = "GET_TODO_BY_ID";
+const DELETE_TODO = "DELETE_TODO";
+const TOGGLE_STATUS_TODO = "TOGGLE_STATUS_TODO";
+
+// Action Creator
+// Todo를 추가하는 action creator
+export const addTodo = (payload) => {
+  return {
+    type: ADD_TODO,
+    payload,
+  };
+};
+
+// Todo를 지우는 action creator
+export const deleteTodo = (payload) => {
+  return {
+    type: DELETE_TODO,
+    payload,
+  };
+};
+
+// Todo를 isDone를 변경하는 action creator
+export const toggleStatusTodo = (payload) => {
+  return {
+    type: TOGGLE_STATUS_TODO,
+    payload,
+  };
+};
+
+// 상세 페이지에서 특정 Todo만 조회하는 action creator
+export const getTodoByID = (payload) => {
+  return {
+    type: GET_TODO_BY_ID,
+    payload,
+  };
+};
+
+// initial state
+const initialState = {
+  todos: [
+    {
+      id: "1",
+      title: "리액트",
+      body: "리액트를 배워봅시다",
+      isDone: false,
+    },
+  ],
+  todo: {
+    id: "0",
+    title: "",
+    body: "",
+    isDone: false,
+  },
+};
+
+const todos = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+
+        // state의 todos 값을 넣어서 기존의 todos가 지워지지 않게 끔 하기
+        todos: [...state.todos, action.payload],
+      };
+
+     // Todo를 지우는 action creator가 switch문 안에 case로 들어있지 않아서 추가시켜 기능이 작동하게 끔 수정
+      case DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      }
+
+    case TOGGLE_STATUS_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id === action.payload) {
+            return {
+              ...todo,
+              isDone: !todo.isDone,
+            };
+          } else {
+            return todo;
+          }
+        }),
+      };
+
+    case GET_TODO_BY_ID:
+      return {
+        ...state,
+        todo: state.todos.find((todo) => {
+          return todo.id === action.payload;
+        }),
+      };
+    default:
+      return state;
+  }
+};
+
+export default todos;
